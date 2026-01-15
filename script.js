02e079bdb329732e7b4eb3af3013dd763189e296
Adam Kopeć <Adam@Adam-WS.(none)>
2026-01-15 12:32:47 +0100

    upd 1.3

---
+++ script.js
@@ -0,0 +1,41 @@
+function logowanie(){
+    const log = document.getElementById('log').value;
+    const haslo = document.getElementById('haslo').value;
+
+    if(log === 'Adam' && haslo === 'Adam'){
+        window.open('zalogowany.html')
+    }
+    else{
+        alert('żle')
+    }
+}
+
+const slides = document.querySelectorAll('.promka');
+let index = 0
+
+function view(n){
+    slides.forEach(promka => promka.classList.remove('active'));
+    slides[n].classList.add('active');
+}
+const left = document.getElementById('left')
+const right = document.getElementById('right')
+right.addEventListener("click", () => {
+    index = (index + 1) % slides.length;
+    view(index)
+});
+left.addEventListener("click", ()=>{
+    index = (index - 1 + slides.length) % slides.length;
+    view(index)
+} );
+
+const burger = document.getElementById('lista');
+const menu = document.getElementById('menu');
+
+burger.addEventListener("click", () => {
+    menu.classList.toggle('active');
+});
+document.addEventListener("click", (e) => {
+    if(!burger.contains(e.target)){
+        menu.classList.remove("active");
+    }
+});
