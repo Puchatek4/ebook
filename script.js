1329a9f81cac5968f85d19959122bb88e2f2da1d
Adam Kopeć <Adam@Adam-WS.(none)>
2026-02-13 23:58:44 +0100

    upd 2.0

--- script.js
+++ script.js
@@ -31,28 +31,9 @@ left.addEventListener("click", ()=>{
 const burger = document.getElementById('lista');
 const menu = document.getElementById('menu');
 
-// burger.addEventListener("click", () => {
-//     menu.classList.toggle('active');
-// });
-// document.addEventListener("click", (e) => {
-//     if(!burger.contains(e.target)){
-//         menu.classList.remove("active");
-//     }
-// });
-// burger.addEventListener("touchstart", () => {
-//     menu.classList.toggle('active');
-// });
-// document.addEventListener("touchstart", (e) => {
-//     if(!burger.contains(e.target)){
-//         menu.classList.remove("active");
-//     }
-// });
-// menu.addEventListener("click", (e) => {
-//     e.preventDefault();
-// });
 function togglemenu(e){
     e.stopPropagation();
-    menu.classList.toggle('active');
+    menu.classList.toggle('active_menu');
 }
 burger.addEventListener('click', togglemenu);
 burger.addEventListener('touchstart', togglemenu);
@@ -61,9 +42,61 @@ menu.addEventListener("click", (e) => {
     e.stopPropagation();
 });
 
+menu.addEventListener("touchstart", (e) => {
+    e.stopPropagation();
+});
+
 document.addEventListener('click', () =>{
-    menu.classList.remove('active');
+    menu.classList.remove('active_menu');
 });
 document.addEventListener('touchstart', () => {
-    menu.classList.remove('active');
+    menu.classList.remove('active_menu');
 })
+
+const darkmode = document.getElementById("darkmode")
+darkmode.addEventListener('click', () => {
+    body.classList.remove('dark');
+});
+
+function create2(){
+    const parentDiv = document.getElementById('rodzic');
+    
+    const newelement = document.createElement('div');
+    newelement.setAttribute('class', 'archiwum');
+    newelement.setAttribute('id', 'pomidor');
+    parentDiv.appendChild(newelement);
+
+    const newH1 = document.createElement('H1');
+    newH1.setAttribute('class', 'archiwum');
+    newH1.innerText = 'Ebook';
+    newelement.appendChild(newH1);
+
+    const newP = document.createElement('P');
+    newP.setAttribute('class', 'podsumowanie');
+    newP.innerText = 'nazwa: ' + '123';
+    newelement.appendChild(newP);
+
+    const newP2 = document.createElement('P');
+    newP2.setAttribute('class', 'podsumowanie');
+    newP2.innerText = 'data zakupu: ' + new Date();
+    newelement.appendChild(newP2);
+
+    const newP3 = document.createElement('P');
+    newP3.setAttribute('class', 'podsumowanie');
+    newP3.innerText = 'Kwota: ';
+    newelement.appendChild(newP3);
+
+    const newP4 = document.createElement('P');
+    newP4.setAttribute('class', 'podsumowanie');
+    newP4.innerText = 'Ilość: ';
+    newelement.appendChild(newP4);
+
+    const newP5 = document.createElement('P');
+    newP5.setAttribute('class', 'podsumowanie');
+    newP5.innerText = 'Dodatkowe informacje: ';
+    newelement.appendChild(newP5);
+
+    const newimg = document.createElement('img');
+    newimg.setAttribute('src', 'planowanie.jpg');
+    newelement.appendChild(newimg);
+}
